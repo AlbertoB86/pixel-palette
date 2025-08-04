47502bcfdce65cbee30e6c3ebbaccdfb478cd40d
Alberto <albertobt86@gmail.com>
2025-08-04 21:03:57 +0200

    Seleccion de colores Vista Previa

--- js/generador.js
+++ js/generador.js
@@ -4,14 +4,20 @@ function iniciar(){
     const boton = document.getElementById('generate-btn');
     const colores = document.getElementById('palette');   
     const ejemplos = document.getElementById('ejemplos');
+
     const toggle = document.getElementById('selector');
     toggle.addEventListener('change', actualizarFormato);  
 
+    const dragInfo = document.getElementById('drag-info');
+    const bombilla = document.getElementById('bombilla');
+    let encendida = false;
+
+    let animacionBombillaActiva = false;
+    let intervaloBombilla;
 
     boton.addEventListener('click', () =>{     
         //Generamos el color base random   
-        const base = chroma.random();        
-        console.log('Base:',base.hex());
+        const base = chroma.random();
 
         //Llamada a la funcion selectArmonia 
         const opcion = document.getElementById('harmony').value;
@@ -19,15 +25,12 @@ function iniciar(){
         
         //Constante para comprobar el estado del HEX o RGB
         const hexRgb = toggle.checked;
-        console.log(hexRgb);
 
         //Generamos la paleta con 5 colores partiendo del color base, y segun la armonia que decidamos
         const escala = chroma.scale([base, armonia]).mode('lab').colors(5);
 
-        console.log('Escala:', escala);
-
         colores.innerHTML = '';
-        ejemplos.innerHTML = '';
+        ejemplos.innerHTML = '';      
 
         /* Bucle para generar los 5 colores de la paleta, creamos la caja
             Añadimos el color al fondo de la caja y ponemos el texto en RGB o HEX segun */ 
@@ -44,27 +47,72 @@ function iniciar(){
             const contraste = chroma.contrast(col, 'white');
             color.style.color = contraste > 4.5 ? 'white' : 'black';
 
+            //Le añadimos a color el atributo para que sea arrastable
+            color.setAttribute('draggable', true);
+            //e.dataTransfer sirve para guardar el valor y recuperarlo al soltar
+            color.addEventListener('dragstart', (e) => {
+                e.dataTransfer.setData('text/plain', escala[i]); 
+            });
+
             colores.appendChild(color);
+            
         }  
-        
-        ejemplos.style.backgroundColor = escala[0];
+
+        const zonas = document.querySelectorAll('.drop-zone');
+
+        zonas.forEach(zona =>{
+            zona.addEventListener('dragover', e => {
+                e.preventDefault(); // Permite soltar
+                zona.style.borderColor = '#fff'; // Feedback visual opcional
+            });
+
+            zona.addEventListener('dragleave', () => {
+                zona.style.borderColor = '#ccc';
+            });
+
+            zona.addEventListener('drop', e =>{
+                e.preventDefault();
+                zona.style.borderColor = '#ccc';
+                const color = e.dataTransfer.getData('text/plain');
+                aplicarColor(zona.dataset.role, color);
+            });
+        });
+
+        const objColores = asignarColores(escala);
+
+        ejemplos.style.backgroundColor = objColores.fondo;
 
         let titulo = document.createElement('h1');
-        titulo.style.color = escala[1];
+        titulo.id = 'titulo-preview';
+        titulo.style.color = objColores.titulo;
         titulo.innerText = 'H1 de Ejemplo';
 
         let texto = document.createElement('p');
-        texto.style.color = escala[2];
+        texto.id = 'texto-preview';
+        texto.style.color = objColores.texto;
         texto.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia tempora excepturi, eos minima exercitationem, facere eaque sit natus sint alias reiciendis! Nulla beatae omnis odit reiciendis repudiandae. Ipsam, asperiores ea.';
 
         let boton = document.createElement('button');
-        boton.style.color = escala[3];
-        boton.style.background = escala[4];
+        boton.id = 'boton-ejemplo';
+        boton.style.color = objColores.botonTexto;
+        boton.style.background = objColores.botonFondo;
         boton.innerText = 'Pulsar';
 
         ejemplos.appendChild(titulo);
         ejemplos.appendChild(texto);
         ejemplos.appendChild(boton);
+
+        //Cambia la bombilla a "on" / "off" cada seg
+        dragInfo.classList.remove('hidden');
+        if (!animacionBombillaActiva){
+            animacionBombillaActiva = true;
+            intervaloBombilla = setInterval(() => {
+                encendida = !encendida;
+                bombilla.src = encendida 
+                    ? './assets/icons/lightbulb-pixel-on.png' 
+                    : './assets/icons/lightbulb-pixel-off.png';
+            },1000);
+        }
     });
 
     // Funcion para seleccionar una Armonia, 
@@ -74,23 +122,18 @@ function iniciar(){
         switch(opcion){
             case 'random':
                 armonia = chroma.random();
-                console.log('Aleatorio:',armonia.hex());
                 break;
             case 'analogous':
                 armonia = base.set('hsl.h', (base.get('hsl.h') + 30) % 360);
-                console.log('Analogo:',armonia.hex());
                 break;
             case 'complementary':
                 armonia = base.set('hsl.h', (base.get('hsl.h') + 180) % 360);
-                console.log('Complementario:',armonia.hex());
                 break;
             case 'monochromatic':
                 armonia = base.set('hsl.l', (base.get('hsl.l') > 0.5 ? base.get('hsl.l') - 0.3 : base.get('hsl.l') + 0.3));
-                console.log('Monocromatica:',armonia.hex());
                 break;
             case 'triadic':
                 armonia = base.set('hsl.h', (base.get('hsl.h') + 120) % 360); 
-                console.log('Triada:',armonia.hex());
                 break;
             default:
                 armonia = chroma.random();
@@ -98,7 +141,8 @@ function iniciar(){
         }
         return armonia;
     }
-
+    
+    // Funcion para cambiar el texto entre HEX o RGB
     function actualizarFormato(){
         const cajas = document.querySelectorAll('.caja');
         const hexRgb = toggle.checked;
@@ -108,5 +152,44 @@ function iniciar(){
             const color = chroma(bg);
             caja.innerText = hexRgb ? color.css() : color.hex();
         });
+    }
+
+    // Funcion para asiganr los valore spor defecto en los ejemplos
+    function asignarColores(escala){
+        //Ordenamos los colores de más oscuro aa más claro
+        const ordenados = [...escala].sort((a, b) => chroma(a).luminance() - chroma(b).luminance());
+
+        return{
+            fondo: ordenados[4],
+            titulo: ordenados[1],
+            texto: ordenados[2],
+            botonFondo: ordenados[3],
+            botonTexto: ordenados[0]
+        }
+    }
+
+    // Funcion para que el usuario pueda cambiar los colores en los ejemplos
+    function aplicarColor(role, color){
+        const titulo = document.getElementById('titulo-preview');
+        const texto = document.getElementById('texto-preview');
+        const boton = document.getElementById('boton-ejemplo');
+
+        switch(role){
+            case 'fondo':
+                ejemplos.style.backgroundColor = color;
+                break;
+            case 'titulo':
+                titulo.style.color = color;
+                break;
+            case 'texto':
+                texto.style.color = color;
+                break;
+            case 'botonFondo':
+                boton.style.background = color;
+                break;
+            case 'botonTexto':
+                boton.style.color = color;
+                break;
+        }
     }
 }
